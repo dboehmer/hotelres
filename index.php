4c81d506c583c8257125b1a5b48e079f217a667d
Daniel Böhmer <it07db1@it2007.ba-leipzig.de>
2009-12-27 18:53:51 +0100

    Create first file index.php and some basic content

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$PAGE_TITLE='Hauptseite';
+$PAGE_HEADLINE='Willkommen bei Hotelsres';
+
+include('include/header.inc');
+
+?>
+
+
+<p>Willkommen bei Hotelres, dem Reservierungssystem für Ihr Hotel oder Ihre Herberge!</p>
+
+<p>Mit diesem System können Reservierungen erstellt, angesehen und verändert werden.</p>
+
+
+<?php
+
+include('include/footer.inc');
+?>
