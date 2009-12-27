efe55182fbf3445e8834586f35be9da6e81c5e7f
Daniel Böhmer <it07db1@it2007.ba-leipzig.de>
2009-12-27 20:08:22 +0100

    Create imprint and fill with basic data about authors

---
+++ imprint.php
@@ -0,0 +1,24 @@
+<?php
+
+$PAGE_TITLE='Impressum';
+$PAGE_HEADLINE='Impressum';
+
+include('include/header.inc');
+
+?>
+
+
+<p>Dieses Projekt wurde im Rahmen eines BA-Studiums erstellt von</p>
+
+<ul>
+<li>Daniel Böhmer</li>
+<li>Patrick Nicolaus</li>
+</ul>
+
+<p>aus der Seminargruppe IT2007.</p>
+
+
+<?php
+
+include('include/footer.inc');
+?>
