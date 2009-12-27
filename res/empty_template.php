ee4ee43637ef7db280a410a7c6c128fe6e64fa7c
Daniel Böhmer <it07db1@it2007.ba-leipzig.de>
2009-12-27 18:52:33 +0100

    Provide empty template for new PHP file in /res/ which can be copied and edited

---
+++ res/empty_template.php
@@ -0,0 +1,17 @@
+<?php
+
+$PAGE_TITLE='some name';
+$PAGE_HEADLINE='some words';
+
+include('include/header.inc');
+
+?>
+
+
+<!-- some content here -->
+
+
+<?php
+
+include('include/footer.inc');
+?>
