6b9c2cb3ee62d19a2e8716ba47d9273349e3b1a0
Daniel Böhmer <it07db1@it2007.ba-leipzig.de>
2009-12-28 14:01:06 +0100

    Add form to schedule.php which enables user to edit details of guest information

--- schedule.php
+++ schedule.php
@@ -5,29 +5,49 @@ $PAGE_HEADLINE='Belegungsplan';
 
 include('include/header.inc');
 
-?>
+
+if ($_GET['show'] == 1)
+	{
+	
+	echo '<form><table>';
+	
+	echo '<tr><td>Name:</td><td><input type="text" name="name" value="Christian Heller"></td></tr>';
+	echo '<tr><td>Anschrift:</td><td><textarea name="address">Schönauer Straße 113a
+	Leipzig</textarea></td></tr>';
+	echo '<tr><td>E-Mail:</td><td><input type="text" name="email" value="christian.heller@ba-leipzig.de"></td></tr>';
+	echo '<tr><td>Telefon:</td><td><input type="text" name="phone" value="0341-1234567"></td></tr>';
+			 
+	echo '</table>';
+	
+	echo '<input type="submit" value="Änderungen speichern">';
+	
+	echo '</form>';
+	
+	}
+
+else
+	{
+
+	echo '<p><a href="schedule.php?show=1">Show single booking edit form</a></p>';
+
+	echo '<h2>Sample</h2>';
+
+	echo '<table><tr><th>Tag</th>';
+	for ($i=1; $i<=7; $i++)
+		{
+		echo "<th>$i.12.2009</th>";
+		}
 
 
-<h2>Sample</h2>
+	echo '</tr><tr><th>Belegung</th>';
+	for ($i=1; $i<=7; $i++)
+		{
+		echo "<td>" . ($i*3) . "%</td>";
+		}
 
-
-
-<?php
-
-echo '<table><tr><th>Tag</th>';
-for ($i=1; $i<=7; $i++)
-    {
-    echo "<th>$i.12.2009</th>";
-    }
-
-
-echo '</tr><tr><th>Belegung</th>';
-for ($i=1; $i<=7; $i++)
-    {
-    echo "<td>" . ($i*3) . "%</td>";
-    }
-
-echo '</tr></table>';
+	echo '</tr></table>';
+	
+	}
 
 include('include/footer.inc');
 
