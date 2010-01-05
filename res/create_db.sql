87602c7af37efe517035932dc03b967892716f76
Daniel Böhmer <it07db1@it2007.ba-leipzig.de>
2010-01-05 22:13:48 +0100

    Update: all tables inserted, fields updated to the result of today's meeting

--- res/create_db.sql
+++ res/create_db.sql
@@ -6,18 +6,26 @@
 DROP TABLE IF EXISTS bookings;
 DROP TABLE IF EXISTS guests;
 DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS users;
 
--- provides basic information about rooms
-CREATE TABLE rooms(
+
+-- gathers information about single bookings
+CREATE TABLE bookings(
 	id INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(id),
-	name VARCHAR(30)
+	room INT NOT NULL,	-- points to rooms.id
+    persons INT NOT NULL,  -- number of persons who will come
+    guest INT NOT NULL,   -- points to guests.id
+	beginn DATE NOT NULL, -- start date
+	end DATE NOT NULL,   -- end date
+	comment varchar(500)   -- free field for entering useful information, e.g. special wishes
 	);
 
+-- holds information about a guest
 CREATE TABLE guests(
     id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id),
-    firstname VARCHAR(50),
+    firstname VARCHAR(50),   -- several types of contact information
     lastname VARCHAR(50),
     street VARCHAR(50),
     number VARCHAR(10),
@@ -28,16 +36,24 @@ CREATE TABLE guests(
     email VARCHAR(50)
     );
 
--- gathers information about single bookings
-CREATE TABLE bookings(
+-- provides basic information about rooms
+CREATE TABLE rooms(
 	id INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(id),
-	room INT NOT NULL,	-- points to rooms.id
-    guest INT NOT NULL,   -- points to guests.id
-	beginn DATE NOT NULL, -- start date
-	end DATE NOT NULL,   -- end date
-	comment varchar(500)   -- free field for entering useful information, e.g. special wishes
+	name VARCHAR(30),   -- name of the room, e.g. "Room 23" / "Appartment 42"
+    capacity INT   -- maximum number of guests in this room
 	);
 
+-- lists all users with password and their rights
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+    username VARCHAR(50) NOT NULL,   -- credentials
+    password VARCHAR(50) NOT NULL,
+    salt VARCHAR(50) NOT NULL,   -- so called salt value for improving password security
+    rights SET("admin","manager","guest")   -- different type of accounts
+    );
 
+-- create default user with admin rights
+INSERT INTO users (username,password) VALUES("admin", "admin");
 
