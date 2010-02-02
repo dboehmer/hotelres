ee0fc9c65f82d8869d6eca0430dd94842d20a60f
Patrick Nicolaus <Patrick.Nicolaus@it2007.ba-leipzig.de>
2010-02-02 22:45:07 +0100

    Edit booking.php. Insert data into table guests, bookings

--- booking.php
+++ booking.php
@@ -41,71 +41,85 @@ else
 		good_query("INSERT INTO guests (firstname,lastname,street,number,zip,city,country,phone,email) VALUES 
 ('".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['street']."','".$_POST['number']."','".$_POST['zip']."','".$_POST['city']."','".$_POST['country']."','".$_POST['phone']."','".$_POST['email']."')",2);
 		
-	        $msg.="<p>".t("Zimmer gebucht.")."</p>";
-			echo $msg;
+		$guest_id = good_last();
+		 
+		$rooms_room = good_query_table("SELECT id, capacity FROM rooms WHERE id='".$_POST['room']."'");		
+		good_query("INSERT INTO bookings (room,guest,persons,begin,end,comment) VALUES 
+('".$rooms_room[0]['id']."','".$guest_id."','".$rooms_room[0]['capacity']."','".$_POST['begin']."','".$_POST['end']."','".$_POST['comment']."')",2);
+
+	    $msg.="<p>".t("Zimmer gebucht.")."</p>";
+		echo $msg;
 	}
 ?>
 
 <form action="booking.php" method="post">
 
 <table>
-<th><table border="0">
-<tr><th><?php echo t("Vorname");?>:</th>
-<td><input type="text" name="firstname"></td></tr>
+	<th><table border="0">
+	<tr><th><?php echo t("Vorname");?>:</th>
+	<td><input type="text" name="firstname"></td></tr>
 
-<tr><th><?php echo t("Nachname");?>:</th>
-<td><input type="text" name="lastname"></td></tr>
+    <tr><th><?php echo t("Nachname");?>:</th>
+    <td><input type="text" name="lastname"></td></tr>
+    
+    <tr><th><?php echo t("Strasse");?>:</th>
+    <td><input type="text" name="street"></td></tr>
 
-<tr><th><?php echo t("Strasse");?>:</th>
-<td><input type="text" name="street"></td></tr>
+    <tr><th><?php echo t("Hausnummer");?>:</th>
+    <td><input type="text" name="number"></td></tr>
+    
+    <tr><th><?php echo t("PLZ");?>:</th>
+    <td><input type="text" name="zip"></td></tr>
+    
+    <tr><th><?php echo t("Wohnort");?>:</th>
+    <td><input type="text" name="city"></td></tr>
+    
+    <tr><th><?php echo t("Land");?>:</th>
+    <td><input type="text" name="country"></td></tr>
+    
+    <tr><th><?php echo t("Telefon");?>:</th>
+    <td><input type="text" name="phone"></td></tr>
+    
+    <tr><th><?php echo t("E-Mail");?>:</th>
+    <td><input type="text" name="email"></td></tr>
+	</table></th>
 
-<tr><th><?php echo t("Hausnummer");?>:</th>
-<td><input type="text" name="number"></td></tr>
+	<th><table border="0">
+    	<tr><th><table border="0">
+    	<tr><th><?php echo t("Datum Einchecken");?>:</th>
+        <td><input type="text" name="begin"></td></tr>
+        
+        <tr><th><?php echo t("Datum Auschecken");?>:</th>
+        <td><input type="text" name="end"></td></tr>
+		</table></th></tr>
+        
+        <tr><th><table border="0">
+		<tr><th><?php echo t("Raum");?>:</th>
+		<td><select name="room" size="1"> 
+        
+		<?php
+        $rooms = good_query_table("SELECT id, name, capacity  FROM rooms",2);
+        foreach($rooms as $room)
+            {
+                echo '<option value="' . $room['id'] . '">' . $room['name'] . "\n(\n" . $room['capacity'] . "\n" . t("Person(en)") . "\n)" . '</option>';
+            }
+        ?>
+        
+        </select></td></tr>
+        
+        </table></th></tr>
+        
+        <tr><th><table border="0">
 
-<tr><th><?php echo t("PLZ");?>:</th>
-<td><input type="text" name="zip"></td></tr>
-
-<tr><th><?php echo t("Wohnort");?>:</th>
-<td><input type="text" name="city"></td></tr>
-
-<tr><th><?php echo t("Land");?>:</th>
-<td><input type="text" name="country"></td></tr>
-
-<tr><th><?php echo t("Telefon");?>:</th>
-<td><input type="text" name="phone"></td></tr>
-
-<tr><th><?php echo t("E-Mail");?>:</th>
-<td><input type="text" name="email"></td></tr>
-
-</table></th>
-<th><table border="0">
-<tr><th><?php echo t("Datum Einchecken");?>:</th>
-<td><input type="text" name="begin"></td></tr>
-
-<tr><th><?php echo t("Datum Auschecken");?>:</th>
-<td><input type="text" name="end"></td></tr>
-
-<tr><th><?php echo t("Kommentar");?>:</th>
-<td><textarea name="comment" rows="5" cols="40"></textarea></td></tr>
-
-<tr><th><?php echo t("Raum");?>:</th>
-<td><select name="room" size="1"> 
-
-<?php
-$rooms = good_query_table("SELECT id, name  FROM rooms",2);
-echo $rooms;
-foreach($rooms as $room)
-	{
-		echo '<option value="' . $room['id'] . '">' . $room['name'] . '</option>';
-	}
-?>
-
-</select></td></tr>
-</table></th>
-
+        <tr><th><?php echo t("Kommentar");?>:</th>
+        <td><textarea name="comment" rows="5" cols="42"></textarea></td></tr>
+        
+        </table></th></tr>
+    </table></th>
 </table>
 
 <input type="submit" value="<?php echo t("Buchen");?>">
+<input type="reset" value="<?php echo t("Reset");?>">
 
 </form>
 
